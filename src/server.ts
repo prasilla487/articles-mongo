cf865dd7dbefab065b573f6023b82d236c768523
Prasilla <prasillamanchem@gmail.com>
2022-08-20 15:30:31 +0530

    added bcrypt

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import App from './app';
 import PostController from './post/postController';
 import './config/path';
 import { validateEnv } from './config/validateEnv';
+import AuthenticationController from './authentication/authenticationController';
 
 validateEnv()
 let post = new PostController()
 
-const app = new App(process.env.PORT, [ post ])
+
+const app = new App(process.env.PORT, [ post , new AuthenticationController()])
 
 app.listen();
